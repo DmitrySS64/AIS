b3c398c089e7eaaaa450a8295e18ce7049ae3013
Dmitry <105486691+DmitrySS64@users.noreply.github.com>
2024-08-21 17:03:04 +0700

    удалил ненужные файлы

--- Lab_1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using ArchitectureOfInformationSystems.MVC.Core;
-using ArchitectureOfInformationSystems.MVC.Model.Entity;
-
-Console.WriteLine("Hello, World!");
-
-new Main<Student>();
